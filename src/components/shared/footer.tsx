b4f5b0b598353ef58d866e1897455862ebe9c87d
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-04-20 19:21:39 +0100

    mobile footer

---
+++ src/components/shared/footer.tsx
@@ -0,0 +1,47 @@
+import styles from '@/styles/shared.module.scss';
+import Image from 'next/image';
+import SocialLinks from './socialLinks';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.get_in_touch}>
+        <h4>Let&apos;s talk about your project</h4>
+        <p>
+          Ready to take it to the next level? Contact us today and find out how our expertise can help your business
+          grow.
+        </p>
+        <button className={styles.btn}>Get in touch</button>
+      </div>
+      <div className={styles.footer_header}>
+        <div className={styles.logo}>
+          <Image src='/assets/shared/desktop/logo-light.png' alt='logo' height={27} width={202} />
+        </div>
+        <ul className={styles.links}>
+          <li>
+            <a href='#'>Our company</a>
+          </li>
+          <li>
+            <a href='#'>Locations</a>
+          </li>
+          <li>
+            <a href='#'>Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.footer_info}>
+        <div className={styles.address}>
+          <p>Designo Central Office</p>
+          <p>3886 Wellington Street</p>
+          <p>Toronto, Ontario M9C 3J5</p>
+        </div>
+        <div className={styles.contact_info}>
+          <p>Contact Us (Central Office)</p>
+          <p>P : +1 253-863-8967</p>
+          <p>M : contact@designo.co</p>
+        </div>
+        <SocialLinks />
+      </div>
+    </footer>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/index/hero';
 import Section1 from '@/components/index/section1';
 import Section2 from '@/components/index/section2';
+import Footer from '@/components/shared/footer';
 import Header from '@/components/shared/header';
 import Head from 'next/head';
 
@@ -18,6 +19,7 @@ export default function Home() {
         <Hero />
         <Section1 />
         <Section2 />
+        <Footer />
       </main>
     </>
   );
