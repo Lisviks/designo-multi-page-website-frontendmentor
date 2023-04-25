180c0689b19aa7a2e45a6cf7f729294ad9a5c044
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-04-25 21:03:34 +0100

    basic form validation

--- src/components/contact/Form.tsx
+++ src/components/contact/Form.tsx
@@ -1,6 +1,31 @@
 import styles from '@/styles/contact/form.module.scss';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import errorIcon from '@/../public/assets/contact/desktop/icon-error.svg';
 
 export default function Form() {
+  const [isNameEmpty, setIsNameEmpty] = useState(false);
+  const [isEmailEmpty, setIsEmailEmpty] = useState(false);
+  const [isPhoneEmpty, setIsPhoneEmpty] = useState(false);
+  const [isMessageEmpty, setIsMessageEmpty] = useState(false);
+
+  const handleChange = (
+    e: React.FormEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>,
+    setIsNameEmpty: Function
+  ) => {
+    const target = e.target as typeof e.target & {
+      value: { length: number };
+      parentElement: HTMLDivElement;
+      id: string;
+    };
+
+    if (target.value.length < 1) {
+      setIsNameEmpty(true);
+    } else {
+      setIsNameEmpty(false);
+    }
+  };
+
   return (
     <section className={styles.form_section}>
       <div className={styles.content}>
@@ -13,20 +38,24 @@ export default function Form() {
       </div>
       <form className={styles.form}>
         <div className={styles.input_field}>
-          <input type='text' id='name' />
+          <input type='text' id='name' onChange={(e) => handleChange(e, setIsNameEmpty)} />
           <label htmlFor='name'>Name</label>
+          {isNameEmpty && <ErrorMessage />}
         </div>
         <div className={styles.input_field}>
-          <input type='email' id='email' />
+          <input type='email' id='email' onChange={(e) => handleChange(e, setIsEmailEmpty)} />
           <label htmlFor='email'>Email Address</label>
+          {isEmailEmpty && <ErrorMessage />}
         </div>
         <div className={styles.input_field}>
-          <input type='text' id='phone' />
+          <input type='text' id='phone' onChange={(e) => handleChange(e, setIsPhoneEmpty)} />
           <label htmlFor='phone'>Phone</label>
+          {isPhoneEmpty && <ErrorMessage />}
         </div>
         <div className={styles.input_field}>
-          <textarea id='message'></textarea>
+          <textarea id='message' onChange={(e) => handleChange(e, setIsMessageEmpty)}></textarea>
           <label htmlFor='message'>Your Message</label>
+          {isMessageEmpty && <ErrorMessage />}
         </div>
         <button type='submit' className={styles.btn}>
           Submit
@@ -34,5 +63,14 @@ export default function Form() {
       </form>
       <div className={styles.bg_pattern}></div>
     </section>
+  );
+}
+
+function ErrorMessage() {
+  return (
+    <span className={styles.error_message}>
+      Can’t be empty
+      <Image src={errorIcon} alt='error icon' />
+    </span>
   );
 }
