d24acb3fcd786947e7bc587afe825c01dee45a8a
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-04-30 06:25:55 +0100

    move viewport meta tag from _document.tsx to _app.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <meta name='description' content='Designo website' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/assets/favicon-32x32.png' />
       </Head>
       <body>
